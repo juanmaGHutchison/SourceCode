d8228baa49c066206d0cfc0663135735519d7241
juanmaGHutchison <fake@fake.com>
2023-07-01 23:13:17 +0200

    TEst broker

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <Arduino.h>
 
 #include "wifiManager/wifiManager.h"
+#include <sMQTTBroker.h>
+
+#define PORT 1883
+
 
 String SSID = "antcolony";
 String password = ""; // 12345678
 
 WifiManager wifiConnection(SSID, password);
+sMQTTBroker broker;
 
 void setup()
 {
   Serial.begin(9600);
   wifiConnection.connectToNetwork();
+
+  broker.init(PORT, true);
 }
 
 void loop()
 {
+  broker.update();
   if (!wifiConnection.isConnected())
   {
     wifiConnection.connectToNetwork();
